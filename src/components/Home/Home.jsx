135a4765f7feeeba5553a38362daffa8017c09e5
Jonathan Rwabahizi <jonathanrwabahizi@gmail.com>
2019-06-17 14:46:22 +0200

    set up repo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Home.scss';
 import '../../assets/css/style.scss';
-import logo from '../../assets/images/logo-01.png'
-import ninja from '../../assets/images/ninja9.png'
+import logo from '../../assets/images/logo-01.png';
+import ninja from '../../assets/images/ninja9.png';
 import dotenv from 'dotenv';
 dotenv.config();
 export default class Home extends React.Component {
@@ -11,85 +11,43 @@ export default class Home extends React.Component {
     const { REACT_APP_URL_BACKEND } = process.env;
     return (
       <div className="grabLandingPage lights">
-
         <div className="row">
           <div className="container">
             <div className="grabName">
               <img src={logo} title="logo" alt="logo" />
-              <br /><br /><br />
+              <br />
+              <br />
+              <br />
               <div className="center-align bold xxlarge-text text-white">
-                <div className="large-padding">
-                  Login to Play
-                </div>
-                <br /><br />
+                <div className="large-padding">Login to Play</div>
+                <br />
+                <br />
                 <a
                   className="social-login radius-4 button danger radius-5 text-white large-h-padding large-text"
-                  href={`${REACT_APP_URL_BACKEND}api/v1/auth/google`}
+                  href={`${REACT_APP_URL_BACKEND}/api/v1/auth/google`}
                 >
                   Google
                 </a>
                 <a
                   className="social-login radius-4 button info radius-5 text-white large-h-padding large-text"
-                  href={`${REACT_APP_URL_BACKEND}api/v1/auth/twitter`}
+                  href={`${REACT_APP_URL_BACKEND}/api/v1/auth/twitter`}
                 >
                   Twitter
                 </a>
                 <a
                   className="social-login radius-4 button primary radius-5 text-white large-h-padding large-text"
-                  href={`${REACT_APP_URL_BACKEND}api/v1/auth/facebook`}
+                  href={`${REACT_APP_URL_BACKEND}/api/v1/auth/facebook`}
                 >
                   Facebook
                 </a>
               </div>
             </div>
             <div className="grabGraphic">
-              <img src={ninja} />
+              <img src={ninja} alt="" />
             </div>
           </div>
         </div>
-        <div className="container hide">
-          <div className="container shadow-2 radius-3">
-            <div className="center">
-              <div className="row">
-                <a
-                  className="radius-4 button primary radius-5 text-white large-h-padding large-text"
-                  href="/game"
-                >
-                  {isAuth ? 'Click here to play The Game' : 'Login to play'}
-                </a>
-                <br />
-                <br />
-                <hr />
-                <div className={isAuth ? 'hide' : ''}>
-                  <br />
-                  <a
-                    className="social-login radius-4 button danger radius-5 text-white large-h-padding large-text"
-                    href="http://localhost:3000/api/v1/auth/google"
-                  >
-                    Google
-                </a>
-                  <a
-                    className="social-login radius-4 button info radius-5 text-white large-h-padding large-text"
-                    href="http://localhost:3000/api/v1/auth/twitter"
-                  >
-                    Twitter
-                </a>
-                  <a
-                    className="social-login radius-4 button primary radius-5 text-white large-h-padding large-text"
-                    href="http://localhost:3000/api/v1/auth/facebook"
-                  >
-                    Facebook
-                </a>
-                  <br />
-                  <br />
-                  <hr />
-                  <br />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div >
+      </div>
     );
   }
 }
