a0dbf1a6845c4908e829626868c1e6fba29ae4a7
Jonathan Rwabahizi <jonathanrwabahizi@gmail.com>
2019-06-17 14:46:22 +0200

    set up repo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import dotenv from 'dotenv';
 import logo from '../../assets/images/ninjas.jpg';
 import './Home.scss';
 import '../../assets/css/style.scss';
 
+dotenv.config();
+
 export default class Home extends React.Component {
   render() {
     const { isAuth } = this.props;
+    console.log(process.env);
+    const { REACT_APP_URL_BACKEND } = process.env;
     return (
       <div className="container App">
         <div className="container shadow-2 radius-3">
           <div className="center">
             <h1 className="primary text-white radius-2 uppercase large-padding center-align">
-            Capital City Challenge
+              Capital City Challenge
             </h1>
             <div className="large-padding">
               <img src={logo} className="App-logo" alt="logo" />
@@ -30,19 +35,19 @@ export default class Home extends React.Component {
                 <br />
                 <a
                   className="social-login radius-4 button danger radius-5 text-white large-h-padding large-text"
-                  href="http://localhost:3000/api/v1/auth/google"
+                  href={`${REACT_APP_URL_BACKEND}/api/v1/auth/google`}
                 >
                   Google
                 </a>
                 <a
                   className="social-login radius-4 button info radius-5 text-white large-h-padding large-text"
-                  href="http://localhost:3000/api/v1/auth/twitter"
+                  href={`${REACT_APP_URL_BACKEND}/api/v1/auth/twitter`}
                 >
                   Twitter
                 </a>
                 <a
                   className="social-login radius-4 button primary radius-5 text-white large-h-padding large-text"
-                  href="http://localhost:3000/api/v1/auth/facebook"
+                  href={`${REACT_APP_URL_BACKEND}/api/v1/auth/facebook`}
                 >
                   Facebook
                 </a>
